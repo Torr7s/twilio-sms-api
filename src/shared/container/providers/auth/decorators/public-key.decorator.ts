a377f81f73d38baafa2c91e6454ebee254bda850
Torr7s <torresmiranda.rj@gmail.com>
2022-03-30 13:13:48 -0300

    Update public-key.decorator.ts
    Removed incorrect typing

--- src/shared/container/providers/auth/decorators/public-key.decorator.ts
+++ src/shared/container/providers/auth/decorators/public-key.decorator.ts
@@ -1,4 +1,4 @@
 import { CustomDecorator, SetMetadata } from '@nestjs/common';
 
 export const IS_PUBLIC_KEY = 'isPublic'
-export const IsPublic: CustomDecorator<string> = (): CustomDecorator<string> => SetMetadata(IS_PUBLIC_KEY, true)
+export const IsPublic = (): CustomDecorator<string> => SetMetadata(IS_PUBLIC_KEY, true)
