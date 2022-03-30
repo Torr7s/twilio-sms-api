51570e9b68ef114edfe16bfcccdc94ba4aa56ceb
Torr7s <torresmiranda.rj@gmail.com>
2022-03-30 12:44:31 -0300

    Update auth-user.dto.ts

--- src/modules/auth/dtos/auth-user.dto.ts
+++ src/modules/auth/dtos/auth-user.dto.ts
@@ -1,4 +1,11 @@
-export interface AuthUserDto {
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class AuthUserDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
