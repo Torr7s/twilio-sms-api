7eb85b2de5f4533e078e4d1ae0ad1c7e9d2e7c23
Torres <torresmiranda.rj@gmail.com>
2022-04-15 11:47:43 -0300

    Added ApiProperty method to show all properties in the documentation

--- src/modules/users/domain/dtos/create-user.dto.ts
+++ src/modules/users/domain/dtos/create-user.dto.ts
@@ -1,18 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @Matches(/^\+[1-9]\d{1,14}$/)
