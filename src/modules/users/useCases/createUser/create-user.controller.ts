cd83fd569481c2779128ad766bb023eae8ee1623
Torres <torresmiranda.rj@gmail.com>
2022-04-15 11:48:47 -0300

    Initialized and finished the documentation for the user tag

--- src/modules/users/useCases/createUser/create-user.controller.ts
+++ src/modules/users/useCases/createUser/create-user.controller.ts
@@ -1,4 +1,6 @@
 import { Body, Controller, Post, Req, Res } from '@nestjs/common';
+import { ApiBody, ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+
 import { Request, Response } from 'express';
 
 import { CreateUserDto } from '@modules/users/domain/dtos/create-user.dto';
@@ -6,14 +8,23 @@ import { CreateUserDto } from '@modules/users/domain/dtos/create-user.dto';
 import { UserEntity } from '@modules/users/infra/typeorm/entities/user.entity';
 
 import { CreateUserService } from './create-user.service';
+import { UserResponseDto } from '@modules/users/domain/dtos/user-response.dto';
 
+@ApiTags('Users')
 @Controller('/api/users/register')
 export class CreateUserController {
   constructor(private readonly _createUserservice: CreateUserService) { }
 
+  @ApiOperation({ description: 'Create an user with the appropriate properties' })
+
+  @ApiBody({ type: CreateUserDto })
+
+  @ApiResponse({ status: 200, description: 'The user has been successfully created', type: UserResponseDto })
+  @ApiResponse({ status: 400, description: 'An user already exists with same email or phone number' })
+
   @Post()
   async handle(
-    @Body() data: CreateUserDto, 
+    @Body() data: CreateUserDto,
     @Req() request: Request,
     @Res() response: Response
   ): Promise<Response> {
