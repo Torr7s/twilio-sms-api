80d18b04253e46d44ebca8f882510a65009ad6d6
Torres <torresmiranda.rj@gmail.com>
2022-04-15 11:27:13 -0300

    Swagger documentation initial setup

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,25 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Twilio SMS Api')
+    .setDescription(
+      'This is a documentation to show how the whole application works'
+    )
+    .setVersion('3.0.0')
+    .addTag('Users')
+    .addTag('Authentication')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(3030)
 }
 
