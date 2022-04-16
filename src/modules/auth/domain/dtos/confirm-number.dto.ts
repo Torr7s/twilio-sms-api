f07ad67bd23acae77f2a5270f06857e603ff737a
Torres <torresmiranda.rj@gmail.com>
2022-04-16 00:48:08 -0300

    Added method to show properties to confirm number

--- src/modules/auth/domain/dtos/confirm-number.dto.ts
+++ src/modules/auth/domain/dtos/confirm-number.dto.ts
@@ -1,11 +1,15 @@
 import { IsNotEmpty, IsString, Matches } from 'class-validator';
 
+import { ApiProperty } from '@nestjs/swagger';
+
 export class ConfirmNumberDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @Matches(/^\+[1-9]\d{1,14}$/)
   phone_number: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   code: string
