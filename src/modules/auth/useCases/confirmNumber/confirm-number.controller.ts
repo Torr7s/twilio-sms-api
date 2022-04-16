2f0760eb95384a9f8e6f9069a048b161f87abdaa
Torres <torresmiranda.rj@gmail.com>
2022-04-16 00:57:04 -0300

    Added methods to complete route documentation

--- src/modules/auth/useCases/confirmNumber/confirm-number.controller.ts
+++ src/modules/auth/useCases/confirmNumber/confirm-number.controller.ts
@@ -1,13 +1,25 @@
 import { Body, Controller, Post, Req, Res } from '@nestjs/common';
+import { ApiOperation, ApiBody, ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
+
 import { Request, Response } from 'express';
 
 import { ConfirmNumberDto } from '@modules/auth/domain/dtos/confirm-number.dto';
 
 import { ConfirmNumberService } from './confirm-number.service';
 
+@ApiTags('Users')
 @Controller('/api/users/confirm')
 export class ConfirmNumberController {
   constructor(private readonly _confirmNumberService: ConfirmNumberService) { }
+
+  @ApiOperation({ description: 'Confirm your phone number being authenticated with JSONWebToken.' })
+
+  @ApiBody({ type: ConfirmNumberDto })
+
+  @ApiResponse({ status: 200, description: 'The phone number has been successfully confirmed' })
+  /* @ApiResponse({ status: 400, description: 'Invalid phone number' }) */
+  @ApiResponse({ status: 400, description: 'Phone number already confirmed', })
+  @ApiResponse({ status: 401, description: 'Invalid code provided to confirm phone number' })
 
   @Post()
   async handle(
