b833337298f7586f907fd147b7002a3284475dcb
Torres <torresmiranda.rj@gmail.com>
2022-04-16 01:03:27 -0300

    Created the last section of the documentation

--- src/modules/auth/useCases/authUser/auth-user.controller.ts
+++ src/modules/auth/useCases/authUser/auth-user.controller.ts
@@ -4,10 +4,19 @@ import { Request, Response } from 'express';
 import { AuthUserDto } from '@modules/auth/domain/dtos/auth-user.dto';
 
 import { AuthUserService } from './auth-user.service';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Authentication')
 @Controller('/api/login')
 export class AuthUserController {
   constructor(private readonly _authUserService: AuthUserService) { }
+
+  @ApiOperation({ description: 'Authenticate yourself to get access to other features' })
+
+  @ApiBody({ type: AuthUserDto })
+
+  @ApiResponse({ status: 200, description: 'Authentication performed successfully' })
+  @ApiResponse({ status: 401, description: 'Invalid credentials provided' })
 
   @Post()
   async handle(
